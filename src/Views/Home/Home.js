4e0872523b4368a3c4bb84c88dd7fff1260874f0
annupatil01 <anushkapatil16003@gmail.com>
2024-08-27 19:28:54 +0530

    client-done

---
+++ src/Views/Home/Home.js
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import PlantCard from '../../Components/PlantCard';
+import { Link } from 'react-router-dom';
+import './Home.css';
+import toast, { Toaster } from 'react-hot-toast';
+import AddICON from "./add-button.png"
+
+function Home() {
+  const [plants, setPlants] = useState([]);
+  const [error, setError] = useState(null);
+
+  const loadPlants = async () => {
+    toast.loading("Loading Plants...");
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/plant`);
+      toast.dismiss();
+      setPlants(response.data.data);
+      toast.success("Plants Loaded Successfully");
+    } catch (err) {
+      toast.dismiss();
+      setError(err.message);
+      toast.error("Failed to load plants");
+    }
+  };
+
+  useEffect(() => {
+    loadPlants();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="brand-name">Trimurti Nursery</h1>
+      <div className="PlantCard-div">
+        {plants.map((plant, i) => {
+          const {
+            _id,
+            plantname,
+            category,
+            image,
+            price,
+            description
+          } = plant;
+
+          return (
+            <PlantCard
+              key={i}
+              _id={_id}
+              plantname={plantname}
+              category={category}
+              image={image}
+              price={price}
+              description={description}
+              loadPlants={loadPlants}
+            />
+          );
+        })}
+        <div>
+        <Link to="/add">
+        <img src={AddICON} className='add-btn' />
+        </Link>
+        </div>
+        <Toaster />
+
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter , RouterProvider } from 'react-router-dom';
+import Home from './Views/Home/Home';
+import AddPlant from './Views/AddPlant/AddPlant';
+import PlantView from './Views/PlantView/PlantView';
+import './index.css';
+import toast ,{ Toaster}  from 'react-hot-toast';
+import UpdatePlant from './Views/UpdatePlant/UpdatePlant';
+
+
+
+
+
+const router = createBrowserRouter([
+ { path: '/',
+  element: <Home/>,
+ }
+,
+
+ {
+  path: '/add',
+  element: <AddPlant/>,
+ },
+  {
+    path: '/update/:id',
+    element: <UpdatePlant/>,
+  },
+
+ {
+  path: '*',
+  element:<h1> 404 NOT FOUND </h1>,
+ }
+])
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<div>
+    <Toaster/>
+<RouterProvider router={router} /></div>);
